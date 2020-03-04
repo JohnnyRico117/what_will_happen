24ec22958d6e57ee1d1acde81e2fa9183048e737
JohnnyRico117 <johnnyrico117@aol.com>
2020-03-04 14:10:12 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/devfreeze/whatwillhappen/what_will_happen/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.devfreeze.whatwillhappen.what_will_happen
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
